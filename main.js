bde55b3b4be9cb63f75617316e991cecc13c016e
Your Name <your.email@example.com>
2025-09-22 01:17:26 +0500

    qoshldi

---
+++ main.js
@@ -0,0 +1,47 @@
+// 1 map
+
+// const array = [1, 4, 9, 16];
+
+// const abu = array.map((element) => element * 3);
+
+// console.log(abu);
+
+// 2 filter
+
+// const words = ["sardor", "saloh", "mirjalol", "mirzohit", "abdulvoris"];
+
+// const abujon = words.filter((word) => word.length > 6);
+
+// console.log(abujon);
+
+// 3 some
+
+// const array = [3, 11, 3, 7, 5];
+
+// const even = (element) => element % 3 === 0;
+
+// console.log(array.some(even));
+
+// 4 Reduce
+
+// const array = [9, 18, 99, 87];
+
+// const initialValue = 0;const sumWithInitial = array.reduce((accumulator, currentValue) => accumulator + currentValue,initialValue,);
+
+// console.log(sumWithInitial);
+
+// 5 every
+
+// const zube = (currentValue) => currentValue < 50;
+
+// const abu = [1, 30, 49, 29, 10, 13];
+
+// console.log(abu.every(zube));
+
+// 6 fint
+
+// const array = [5, 8, 12, 130, 44];
+
+// const end = array.find((element) => element > 7);
+
+// console.log(end);
